c0a0ba251f0343e03354c956b463d5db9d876046
gnijhawan2016 <gnijhawan2016@gmail.com>
2021-03-08 16:56:40 +0400

    3 files modified, 3 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,11 +13,9 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared!');
   }
+
+  onNotify(product) {
+    window.alert("You will be notified when the product "+ product.id + " goes on sale!")
+  }
+
 }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
